ec87cef49cec085fbee98b7357fe64bd8d6a3678
Unknown <jeremie.de20@gmail.com>
2017-10-02 15:33:38 +0200

    5

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<header>
+	<nav class="navbar navbar-inverse bg-primary navbar-toggleable-md navbar-light bg-faded">
+		  
+		<a class="navbar-brand" href="#">
+		  	Nom du commercial
+		  	<img src="" width="30" height="30" alt="">
+		</a>
+
+		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		</button>
+
+	</nav>
+</header>
