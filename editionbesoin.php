5281d00f1f4fe819d70903c45e1b02ba9acd84fa
Unknown <jeremie.de20@gmail.com>
2017-10-02 15:22:52 +0200

    4

---
+++ editionbesoin.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<?php include('includes/head.php'); ?>
+</head>
+<body>
+
+	<header>
+		<nav class="navbar navbar-inverse bg-primary navbar-toggleable-md navbar-light bg-faded">
+		  
+		  <a class="navbar-brand" href="#">
+		  	Nom du commercial
+		  	<img src="" width="30" height="30" alt="">
+		  </a>
+
+		  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+
+		</nav>
+	</header>
+
+
+
+</body>
+</html>
